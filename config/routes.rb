5a217d0af4f6093e0dbb343b48f4df0ac39ac4e7
GiovanniGiunti <giunti.1804643@studenti.uniroma1.it>
2020-05-03 16:57:02 +0200

    commento

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	#get ('music') => redirect('/')
-	#post ('/music/:id') => "music#update"
-	#post ('/music/new') => "music#create"
-	resources :music
-end
