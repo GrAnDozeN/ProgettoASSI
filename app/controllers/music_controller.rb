3b560bea404dbf2f9e3d3a2f64ece4c46bacd3a3
GrAnDozeN <cramilo55@gmail.com>
2020-05-06 19:33:42 +0200

    Implementato Bootstrap alle pagine Music \n Aggiunto controllo dei parametri in music#new!

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -7,20 +7,26 @@ skip_before_action :verify_authenticity_token
 	end
 
 	def create
-		@music = Music.create!(params[:music].permit(:title,:author,:album,:genre,:year))
-		redirect_to music_index_path
+		if !(Music.exists?(:title=>params[:music][:title], :author=>params[:music][:author], :genre=>params[:music][:genre], :year=>params[:music][:year]))
+			@music = Music.create!(params[:music].permit(:title,:author,:album,:genre,:year))
+			redirect_to music_path(@music)
+		else
+			redirect_to music_index_path
+		end
 	end
 
 	def show
-        	id = params[:id]
-        	if Music.exists?(id)
-            		@music = Music.find(id)
-        	else
-           	render html: 'Track does not exit'
-        	end
+		@currentUser = Musicgoer.find(session[:user_id])
+        id = params[:id]
+        if Music.exists?(id)
+        		@music = Music.find(id)
+        else
+        render html: 'Track does not exit'
+        end
 	end
 
 	def new
+		@currentUser = Musicgoer.find(session[:user_id])
 	end
 
 	def destroy
@@ -38,6 +44,7 @@ skip_before_action :verify_authenticity_token
     end
 
 	def edit
+		@currentUser = Musicgoer.find(session[:user_id])
 		id = params[:id]
 		@music = Music.find(id)
 	end
