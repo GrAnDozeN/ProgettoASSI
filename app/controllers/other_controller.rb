3cad3395c99cebbe51c6e3ed2f92f7a817b39366
GrAnDozeN <cramilo55@gmail.com>
2020-06-26 08:54:17 +0200

    Last fix

--- app/controllers/other_controller.rb
+++ app/controllers/other_controller.rb
@@ -1,9 +1,9 @@
 class OtherController < ApplicationController
     
     def about        
-        @dev01 = {:name => "name", :surname => "surname", :pic => "https://www.kindpng.com/picc/b/11/113191.png", :description => "Christian Realin Ramilo"}
-        @dev02 = {:name => "name", :surname => "surname", :pic => "https://www.kindpng.com/picc/b/11/113191.png", :description => "Giovanni Giunti"}
-        @dev03 = {:name => "name", :surname => "surname", :pic => "https://www.kindpng.com/picc/b/11/113191.png", :description => "Giancarlo Liu"}
+        @dev01 = {:name => "Christian Realin", :surname => "Ramilo", :pic => "https://www.kindpng.com/picc/b/11/113191.png", :description => "Christian Realin Ramilo"}
+        @dev02 = {:name => "Giovanni", :surname => "Giunti", :pic => "https://www.kindpng.com/picc/b/11/113191.png", :description => "Giovanni Giunti"}
+        @dev03 = {:name => "Giancarlo", :surname => "Liu", :pic => "https://www.kindpng.com/picc/b/11/113191.png", :description => "Giancarlo Liu"}
         @listDev = [@dev01, @dev02, @dev03]
     end
 
